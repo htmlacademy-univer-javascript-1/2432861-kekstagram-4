35eaf41e21ac4160bb5e161d22869509f655e6e9
Keks <keks@htmlacademy.ru>
2023-09-30 15:28:17 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/function.js
@@ -0,0 +1,23 @@
+const checkStringLength = (string, maxStringLength) => string.length <= maxStringLength;
+
+const isPalindromString = (string) => {
+  string = string.toLowerCase().split(' ').join('');
+  const reversedString = string.split('').reverse().join('');
+  return string === reversedString;
+};
+
+const findNumbers = (input) => {
+  const inputString = input.toString();
+  let numbers = '';
+
+  for (const char of inputString) {
+    if (char >= '0' && char <= '9') {
+      numbers += char;
+    }
+  }
+  return numbers === '' ? NaN : +numbers;
+};
+
+checkStringLength('проверяемая строка', 20);
+isPalindromString('Лёша на полке клопа нашёл ');
+findNumbers('1 кефир, 0.5 батона');
