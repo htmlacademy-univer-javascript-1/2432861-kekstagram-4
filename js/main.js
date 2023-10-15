66084769ec3b3bc7dd8d7c7d05dd86593c4c2477
ZiminMisha <mikhail.zimin.2004@bk.ru>
2023-10-15 21:04:42 +0500

    refactoring 4.16

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
-function getRandomInteger(min, max) {
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const NAMES = ['Александр', 'Екатерина', 'Иван', 'Мария', 'Дмитрий', 'Анна'];
+const MAX_COMMENT_ID = 1000;
+const MAX_PHOTO_ID = 25;
+const MIN_LIKES_COUNT = 15;
+const MAX_LIKES_COUNT = 200;
+const MAX_COMMENTS_COUNT = 30;
+
+const getRandomInteger = (min, max) => {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
   const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
   const result = Math.random() * (upper - lower + 1) + lower;
 
   return Math.floor(result);
-}
+};
 
-function createRandomIdFromRangeGenerator(min, max) {
+const createRandomIdFromRangeGenerator = (min, max) => {
   const previousValues = [];
 
   return function () {
     let currentValue = getRandomInteger(min, max);
     if (previousValues.length >= (max - min + 1)) {
-      // console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
-
       return null;
     }
     while (previousValues.includes(currentValue)) {
@@ -23,57 +36,46 @@ function createRandomIdFromRangeGenerator(min, max) {
 
     return currentValue;
   };
-}
+};
 
-const generateCommentId = createRandomIdFromRangeGenerator(1, 1000);
-const generatePhotoId = createRandomIdFromRangeGenerator(1, 25);
+const generateCommentId = createRandomIdFromRangeGenerator(1, MAX_COMMENT_ID);
+const generatePhotoId = createRandomIdFromRangeGenerator(1, MAX_PHOTO_ID);
 
-const photos = [];
-
-function generateRandomComment() {
-  const messages = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
-
-  const names = ['Александр', 'Екатерина', 'Иван', 'Мария', 'Дмитрий', 'Анна'];
-
+const generateRandomComment = () => {
   const comment = {
     id: generateCommentId(),
     avatar: `photos/${getRandomInteger(1, 6).toString()}.jpg`,
-    message: messages[getRandomInteger(0, messages.length - 1)],
-    name: names[getRandomInteger(0, names.length - 1)],
+    message: MESSAGES[getRandomInteger(0, MESSAGES.length - 1)],
+    name: NAMES[getRandomInteger(0, NAMES.length - 1)],
   };
 
   return comment;
-}
+};
 
-function createPhoto() {
+const createPhoto = () => {
   const photoId = generatePhotoId();
   const photo = {
     id: photoId,
-    url: `photos/${photoId.toString()}.png`,
-    description: `Описание ${photoId.toString()}-ой фотографии`,
-    likes: getRandomInteger(15, 200),
+    url: `photos/${photoId}.png`,
+    description: `Описание ${photoId}-ой фотографии`,
+    likes: getRandomInteger(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
     comments: []
   };
 
-  const numComments = getRandomInteger(0, 30);
+  const numComments = getRandomInteger(0, MAX_COMMENTS_COUNT);
   for (let i = 0; i < numComments; i++) {
     photo.comments.push(generateRandomComment());
   }
 
   return photo;
-}
+};
 
-for (let i = 1; i <= 25; i++) {
-  photos.push(createPhoto());
-}
+const createPhotos = () => {
+  const photos = [];
 
-// console.log(photos);
-// console.log(photos[0].comments);
+  for (let i = 1; i <= MAX_PHOTO_ID; i++) {
+    photos.push(createPhoto());
+  }
 
+  return photos;
+};
