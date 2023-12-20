ef52891711baba80cd3079c1e9215dcba8954b1e
ZiminMisha <mikhail.zimin.2004@bk.ru>
2023-12-20 14:53:40 +0500

    12.12. Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ import { renderPhotos } from './thumbnails.js';
 import { initEditPopup } from './edit-popup.js';
 import { createLoader, ErrorText } from './api.js';
 import { showErrorLoadImgMessage } from './error-load-data-server.js';
+import { initFilters } from './filters.js';
+import { debounce } from './utils.js';
 
 const onSuccess = (data) => {
   renderPhotos(data);
+  initFilters(data, debounce(renderPhotos));
   initEditPopup();
 };
 
